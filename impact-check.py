63c0f2b041f761d8983268ecbe026cf686ca5976
Tomáš Hrnčiar <thrnciar@redhat.com>
2023-04-24 08:28:08 +0200

    Parse URL address, clone package repo, apply patch

--- impact-check.py
+++ impact-check.py
@@ -14,8 +14,9 @@ number>.patch")
 args = parser.parse_args()
 
 try:
-    package_name = re.search("(?<=rpms\/)(.*)(?=\/pull-request)", args.url)
-    path = f"/tmp/{package_name.group(0)}"
+    package_name = re.search("(?<=rpms\/)(.*)(?=\/pull-request)", args.url).group(0)
+    path = f"/tmp/{package_name}"
+    pr_number = args.url.split('/')[-1]
 except AttributeError:
     print("Given URL address doesn't match expected format, see --help.")
     exit(1)
@@ -23,8 +24,16 @@ except AttributeError:
 # Create working directory
 os.mkdir(path)
 
+fedpkg = f"fedpkg clone {package_name} {path}"
+subprocess.run(shlex.split(fedpkg))
+
+os.chdir(path)
+
 wget = f"wget -P {path} {args.url}"
 subprocess.run(shlex.split(wget))
+
+git_apply = f"git apply {pr_number}"
+subprocess.run(shlex.split(git_apply))
 
 # Clean up, remove working directory
 shutil.rmtree(path)
