c4dcdcb7012759ad7782dd05744b9995184e062b
Tomáš Hrnčiar <thrnciar@redhat.com>
2023-04-24 09:11:17 +0200

    Create new copr

--- impact-check.py
+++ impact-check.py
@@ -3,14 +3,19 @@
 import argparse
 import os
 import re
+import secrets
 import shlex
 import shutil
 import subprocess
 
 parser = argparse.ArgumentParser()
+parser.add_argument("copr", help="Copr user/@group where the testing Copr \
+should be created")
 parser.add_argument("url", help="Link to the PR that should be impact checked. \
 Eg, https://src.fedoraproject.org/rpms/<package name>/pull-request/<PR \
 number>.patch")
+parser.add_argument("--chroot", default="fedora-rawhide-x86_64", help="What chroot \
+should be used in the testing Copr, the default is fedora-rawhide-x86_64")
 args = parser.parse_args()
 
 try:
@@ -34,6 +39,10 @@ subprocess.run(shlex.split(wget))
 
 git_apply = f"git apply {pr_number}"
 subprocess.run(shlex.split(git_apply))
+
+uuid = secrets.token_hex(4)
+new_copr = f"copr-cli create {args.copr}/{package_name}-{uuid} --chroot {args.chroot}"
+subprocess.run(shlex.split(new_copr))
 
 # Clean up, remove working directory
 shutil.rmtree(path)
